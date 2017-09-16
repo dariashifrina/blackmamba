37a036b6cf8bb74e97d9f7035665462123666efc
Dasha <dasha@ubuntu.ubuntu-domain>
2017-09-16 18:44:54 -0400

    2 euler problems

--- multiples.c
+++ multiples.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-  int threes = (333.0/2) * 1003;
-  int fives = (200/2) * 1005;
-  int fifteens = (66/2) * 1015;
+  int threes = (333.0/2) * 1002;
+  int fives = (199.0/2) * 1000;
+  int fifteens = (66/2) * 1005;
   int sum = threes + fives - fifteens;
   printf("Sum of multiples of three and five up to 1000:%d\n",sum);
   return 0;
